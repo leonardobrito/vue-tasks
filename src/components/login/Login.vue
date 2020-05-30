69060c98054cdbcc6dbc7cdeec838d3a15228662
Thiago Brasil <thiago.brasil@sitiobarreiras.com.br>
2020-05-30 10:47:48 -0300

    one way data binding to username and password

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -3,18 +3,29 @@
     <h1>Login</h1>
     <div>
       <label>Username:</label>
-      <input type="text" />
+      <input type="text" @input="username = $event.target.value" />
     </div>
     <div>
       <label>password:</label>
-      <input type="password" autocomplete="new-password" />
+      <input
+        type="password"
+        autocomplete="new-password"
+        @input="password = $event.target.value"
+      />
     </div>
     <button>Login</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
 </script>
 
 <style></style>
