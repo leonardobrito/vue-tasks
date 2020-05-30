93a105d53e12ddfae05c1a1cd97895ed4fe1fe99
Thiago Brasil <thiago.brasil@sitiobarreiras.com.br>
2020-05-30 13:44:52 -0300

    adding page ListTaskgroup

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Login from "./components/login/Login.vue";
+import ListTaskgroup from "./components/taskgroup/list-taskgroup/ListTaskgroup.vue";
 
 export const routes = [
   {
     name: "login",
     path: "/login",
     component: Login
+  },
+  {
+    name: "listTaskgroup",
+    path: "/taskgroup",
+    component: ListTaskgroup
   }
 ];
