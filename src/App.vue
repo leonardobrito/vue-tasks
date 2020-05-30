d625c18cdc17e853f03bb0ac300ec806a6dc8849
Thiago Brasil <thiago.brasil@sitiobarreiras.com.br>
2020-05-30 10:30:30 -0300

    add component with form Login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <login></login>
   </div>
 </template>
 
 <script>
+import Login from "./components/login/Login";
+
 export default {
   name: "app",
+  components: {
+    login: Login
+  },
   data() {
     return {
       msg: "Hello World"
---
+++ src/components/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <label>Username:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label>password:</label>
+      <input type="password" autocomplete="new-password" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
