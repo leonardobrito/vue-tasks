8caf310dbb5d20bc0957fbf3c121128e52c5787e
Thiago Brasil <thiago.brasil@sitiobarreiras.com.br>
2020-05-30 10:52:21 -0300

    change inputs to use two way data binding

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -3,15 +3,11 @@
     <h1>Login</h1>
     <div>
       <label>Username:</label>
-      <input type="text" @input="username = $event.target.value" />
+      <input type="text" v-model="username" />
     </div>
     <div>
       <label>password:</label>
-      <input
-        type="password"
-        autocomplete="new-password"
-        @input="password = $event.target.value"
-      />
+      <input type="password" autocomplete="new-password" v-model="password" />
     </div>
     <button>Login</button>
   </div>
