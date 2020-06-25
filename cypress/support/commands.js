1294cb868dfd0fa90c1454a61cfeb1d52dcd07ef
Leonardo Silva <f.leobrito@gmail.com>
2020-06-25 01:47:10 -0300

    :white_check_mark: Adding more cypress tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (username, password) => {
+  cy.visit("/");
+  cy.get("input[type=text]").clear();
+  cy.get("input[type=text]").type(username);
+  cy.get("input[type=password]").clear();
+  cy.get("input[type=password]").type(password);
+  cy.get("button").click();
+});
+
+Cypress.Commands.add("createTaskGroupList", (name) => {
+  cy.contains('New Taskgroup').click();
+  cy.get("input[name='name']").type(name);
+  cy.get("input[name='description']").type(`description ${name}`);
+  cy.get("select").select('daily');
+  cy.contains('Save').click();
+});
